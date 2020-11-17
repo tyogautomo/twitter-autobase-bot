c683e20e35beae9d2f093f5c59766e6b3e584ea1
tyogautomo <yoga.utomo27@gmail.com>
2020-11-17 14:45:36 +0700

    Part 4

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const CronJob = require('cron').CronJob;
 const app = express();
 
 const { TwitterBot } = require('./twitter-bot');
@@ -6,11 +7,22 @@ const { TwitterBot } = require('./twitter-bot');
 const PORT = 3000;
 
 const twitterBot = new TwitterBot({
-    consumer_key: 'asd',
-    consumer_secret: 'asd',
-    access_token: 'asd',
-    access_token_secret: 'asd'
-})
+    consumer_key: 'R4xDiwGr9mc6JE82guyBcgFS7',
+    consumer_secret: '5n8L5FalVW7BW6WDgzPY2HfDtnmX2arD9rdUpKaIafzChWuCvl',
+    access_token: '67367056-Yi5v5POJvxDKo55uGxu5ZeBYhiRGtoLQHRiLwpOqF',
+    access_token_secret: '6VdqBJI98ODsdVqumtsRxSGdOBy9v15Se3VvVXSodAZSF'
+});
+
+const job = new CronJob(
+    '*/2 * * * * *',
+    doJob,
+    null,
+    false
+);
+
+function doJob() {
+    console.log('ketrigger nih...!');
+};
 
 app.get('/', (req, res, next) => {
     res.send('Welcome to twitter bot server!');
