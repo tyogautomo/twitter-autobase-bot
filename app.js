3000b91c31acb635a84fa37cb5ae09ceaf62d730
tyogautomo <yoga.utomo27@gmail.com>
2021-03-18 13:33:37 +0700

    add cron to interval 3 minutes

--- app.js
+++ app.js
@@ -15,33 +15,32 @@ const bot = new TwitterBot({
 });
 
 const job = new CronJob(
-    '*/1 * * * * *',
+    '* */3 * * * *',
     doJob,
     null,
-    true
+    false
 );
 
 async function doJob() {
-    console.log('tick');
-    // let tempMessage;
-    // try {
-    //     const authenticatedUserId = await bot.getAdminUserInfo();
-    //     const message = await bot.getDirectMessage(authenticatedUserId);
-    //     if (message.id) {
-    //         tempMessage = message;
-    //         await bot.tweetMessage(message);
-    //         await bot.deleteMessage(message);
-    //         console.log('message has been deleted from twitter....');
-    //     } else {
-    //         console.log('no tweet to post --------------------------');
-    //     }
-    // } catch (error) {
-    //     console.log(error);
-    //     console.log('--------------- ERROR ------------------');
-    //     if (tempMessage.id) {
-    //         await bot.deleteMessage(tempMessage);
-    //     }
-    // }
+    let tempMessage;
+    try {
+        const authenticatedUserId = await bot.getAdminUserInfo();
+        const message = await bot.getDirectMessage(authenticatedUserId);
+        if (message.id) {
+            tempMessage = message;
+            await bot.tweetMessage(message);
+            await bot.deleteMessage(message);
+            console.log('message has been deleted from twitter....');
+        } else {
+            console.log('no tweet to post --------------------------');
+        }
+    } catch (error) {
+        console.log(error);
+        console.log('--------------- ERROR ------------------');
+        if (tempMessage.id) {
+            await bot.deleteMessage(tempMessage);
+        }
+    }
 };
 
 app.get('/', (req, res, next) => {
